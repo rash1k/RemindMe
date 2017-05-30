dc5b2ba06633f33101f61957c0a4630aa16bfd0a
rash1k <dmitriyrash1k@gmail.com>
2017-05-30 23:02:14 +0300

    Start to implementing NavigationView.

--- app/src/main/java/ua/example/rash1k/remindme/MainActivity.java
+++ app/src/main/java/ua/example/rash1k/remindme/MainActivity.java
@@ -3,12 +3,33 @@ package ua.example.rash1k.remindme;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_AppThemeDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        mToolbar.inflateMenu(R.menu.menu);
     }
 }
